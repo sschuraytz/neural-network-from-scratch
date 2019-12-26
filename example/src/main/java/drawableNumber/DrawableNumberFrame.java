89ff9de3c6ce9fdbedc392aaf5cb26d8f810f1c7
sschuraytz <sbschuraytz@gmail.com>
2019-12-26 16:30:46 -0500

    created basic setup with clear button. however, running into issue drawing bufferedImage onto a panel that's in another panel

--- example/src/main/java/drawableNumber/DrawableNumberFrame.java
+++ example/src/main/java/drawableNumber/DrawableNumberFrame.java
@@ -1,17 +1,43 @@
 package drawableNumber;
 import javax.swing.*;
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Dimension;
 
-public class DrawableNumberFrame {
+public class DrawableNumberFrame extends JFrame {
+
+    private JButton submitButton = new JButton("Find my Number");
+    private JButton clearButton = new JButton("Clear");
+    private DrawingComponent drawingComponent = new DrawingComponent();
+    private JPanel drawingPanel = new JPanel();
+   // private JLabel drawingLabel = new JLabel("ok");
 
     public DrawableNumberFrame() {
         setTitle("Draw A Number");
-        setSize(600, 600);
-        setDefaultCloesOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(300, 300);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        JPanel root = new JPanel();
+        root.setLayout(new BorderLayout());
+        JPanel buttonPanel = new JPanel();
 
+        JTextField outputText = new JTextField("You Drew: ");
+        outputText.setEditable(false);
+        buttonPanel.add(submitButton);
+        buttonPanel.add(clearButton);
+        buttonPanel.add(outputText);
+        root.add(buttonPanel, BorderLayout.SOUTH);
+
+        //potential issue: drawingPanel is initially empty b/c it has nothing.
+//        drawingLabel.setIcon(new ImageIcon(drawingComponent.getImage()));
+//        root.add(drawingLabel, BorderLayout.NORTH);
+        drawingPanel.add(drawingComponent);
+        drawingPanel.revalidate();
+        drawingPanel.repaint();
+       // root.add(drawingPanel);
+       // root.add(drawingComponent);
+
+        setContentPane(root);
+        root.setVisible(true);
+
+        clearButton.addActionListener(e -> drawingComponent.clearImage());
     }
 }
--- example/src/main/java/drawableNumber/DrawingComponent.java
+++ example/src/main/java/drawableNumber/DrawingComponent.java
@@ -1,4 +1,56 @@
 package drawableNumber;
+import javax.swing.JComponent;
+import javax.swing.event.MouseInputAdapter;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
+import java.awt.geom.Line2D;
+import java.awt.image.BufferedImage;
+import java.awt.Point;
+import java.awt.Shape;
 
-public class DrawingComponent {
+public class DrawingComponent extends JComponent {
+    private BufferedImage buffImage = new BufferedImage(300, 300, BufferedImage.TYPE_INT_ARGB);
+    private Point startPoint;
+    private Point endPoint;
+    private Shape line;
+
+    public DrawingComponent() {
+        MouseInputAdapter mouseInputAdapter = new MouseInputAdapter() {
+            public void mousePressed(MouseEvent e) {
+                startPoint = e.getPoint();
+            }
+            public void mouseDragged(MouseEvent e) {
+                endPoint = e.getPoint();
+                line = new Line2D.Double(startPoint, endPoint);
+                startPoint = endPoint;
+                drawLines();
+            }
+        };
+        addMouseListener(mouseInputAdapter);
+        addMouseMotionListener(mouseInputAdapter);
+    }
+
+    public void drawLines() {
+        Graphics2D g = (Graphics2D) buffImage.getGraphics();
+        g.setColor(Color.BLACK);
+        g.draw(line);
+        repaint();
+    }
+
+    public void clearImage() {
+        Graphics2D g2d = (Graphics2D) buffImage.getGraphics();
+        g2d.setBackground(new Color(0, 0, 0, 0));
+        g2d.clearRect(buffImage.getMinX(), buffImage.getMinY(), buffImage.getWidth(), buffImage.getHeight());
+        repaint();
+    }
+
+    @Override
+    public void paintComponent(Graphics graphics) {
+        super.paintComponent(graphics);
+        if (buffImage != null) {
+            graphics.drawImage(buffImage, 0, 0, null);
+        }
+    }
 }
