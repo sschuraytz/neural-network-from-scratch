e11794b7e17eb864b74af2893c427c44a8da3bf4
sschuraytz <sbschuraytz@gmail.com>
2019-12-26 14:53:21 -0500

    created side by side panels for user drawing input and for computer generated output

---
+++ example/src/main/java/andrewoid/neuralnetwork/drawablenumber/DrawableNumberFrame.java
@@ -0,0 +1,43 @@
+package andrewoid.neuralnetwork.drawablenumber;
+import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Dimension;
+
+public class DrawableNumberFrame extends JFrame {
+
+    public DrawableNumberFrame() {
+        setTitle("Draw a Number");
+        setSize(600, 600);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        JPanel root = new JPanel();
+        root.setLayout(new BorderLayout());
+        JButton clearButton = new JButton("Clear");
+        root.add(clearButton, BorderLayout.SOUTH);
+        root.setBackground(Color.GRAY);
+
+        JPanel drawablePanel = new JPanel();
+        JPanel outputPanel = new JPanel();
+        JLabel drawableLabel = new JLabel("Draw Your Number Here");
+        drawableLabel.setFont(new Font("Sans Serif", Font.BOLD, 16));
+        JLabel outputLabel = new JLabel("Here is Your Number");
+        outputLabel.setFont(new Font("Sans Serif", Font.BOLD, 16));
+        drawablePanel.add(drawableLabel, BorderLayout.EAST);
+        outputPanel.add(outputLabel, BorderLayout.CENTER);
+        drawablePanel.setBackground(Color.PINK);
+        outputPanel.setBackground(Color.YELLOW);
+
+        root.add(drawablePanel, BorderLayout.WEST);
+        root.add(outputPanel, BorderLayout.EAST);
+
+        DrawingComponent drawingComponent = new DrawingComponent();
+//        drawablePanel.add(drawingComponent);
+        root.add(drawingComponent);
+        drawablePanel.add(drawingComponent);
+
+        setContentPane(root);
+        root.setVisible(true);
+    }
+}
