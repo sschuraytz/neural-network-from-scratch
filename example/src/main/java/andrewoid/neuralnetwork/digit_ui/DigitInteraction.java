ae63e36807e42786d1120ded4085ef9fddc62749
Schwimmer <aschwimm@admin.touro.edu>
2020-01-02 19:56:01 -0500

    fixed merge conflict

--- example/src/main/java/andrewoid/neuralnetwork/digit_ui/DigitInteraction.java
+++ example/src/main/java/andrewoid/neuralnetwork/digit_ui/DigitInteraction.java
@@ -1,11 +1,13 @@
 package andrewoid.neuralnetwork.digit_ui;
 
 import andrewoid.neuralnetwork.Network;
-import andrewoid.neuralnetwork.Neuron;
+import andrewoid.neuralnetwork.NetworkFactory;
+
+import java.io.FileNotFoundException;
 
 public class DigitInteraction {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Network network = new NetworkFactory().loadFromJSON("network.json");
         new DigitFrame(network).setVisible(true);
     }
