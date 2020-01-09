04adcc0d1ce714cdc347aa57079a35892b78f9e0
Schwimmer, Andrew <Andrew.Schwimmer@mlb.com>
2020-01-09 14:18:59 -0500

    removed testing code from MINSTTraining.java, changed the output file and loaded it if it exists

--- example/src/main/java/andrewoid/neuralnetwork/MNISTReader/MNISTTraining.java
+++ example/src/main/java/andrewoid/neuralnetwork/MNISTReader/MNISTTraining.java
@@ -1,21 +1,21 @@
 package andrewoid.neuralnetwork.MNISTReader;
 
 import andrewoid.neuralnetwork.Network;
+import andrewoid.neuralnetwork.NetworkFactory;
 import andrewoid.neuralnetwork.Neuron;
 
+import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Random;
 
 public class MNISTTraining
 {
+    private static final String saveFilename = "mnist.json";
     private static final String trainingInputImagePath = "train-images-idx3-ubyte";
     private static final String trainingInputLabelPath = "train-labels-idx1-ubyte";
-    private static final String testingInputImagePath = "t10k-images-idx3-ubyte";
-    private static final String testingInputLabelPath = "t10k-labels-idx1-ubyte";
 
     private static final String trainingOutputPath = "TrainingMNISTOutputFiles/";
-    private static final String testingOutputPath = "TestingMNISTOutputFiles/";
 
     private static final double expectedOutputs[][] = new double[][]{
             {1, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // 0
@@ -34,8 +34,12 @@ public class MNISTTraining
     {
         IdxReader reader = new IdxReader(trainingInputImagePath,trainingInputLabelPath,trainingOutputPath);
 
-        Network network = new Network(784, 1600, 1600, 10);
-        Neuron outputs[];
+        Network network;
+        try {
+            network = new NetworkFactory().loadFromJSON(saveFilename);
+        } catch (FileNotFoundException e) {
+            network =new Network(784, 15, 15, 10);
+        }
 
         Iterator<MNISTTrainingFile> iterator = reader.iterator();
         while(iterator.hasNext())
@@ -44,28 +48,7 @@ public class MNISTTraining
             network.train(next.getImgPixelsAsDoubles(), expectedOutputs[next.getLabel()], 0.3);
         }
 
-
-        reader.resetInputImageAndFilePath(testingInputImagePath, testingInputLabelPath, testingOutputPath);
-        Iterator<MNISTTrainingFile> iterator2 = reader.iterator();
-
-        while(iterator2.hasNext())
-        {
-            MNISTTrainingFile next = iterator2.next();
-            double[] imgInput = next.getImgPixelsAsDoubles();
-            outputs = network.evaluate(imgInput);
-            System.out.print(Arrays.toString(imgInput));
-            System.out.print(" = ");
-            for (int i = 0; i < outputs.length; i++) {
-                double value = outputs[i].getValue();
-                if (value > 0.10) {
-                    System.out.print(i);
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
-
-        network.saveToJSON("network.json");
+        network.saveToJSON(saveFilename);
     }
 
 }
